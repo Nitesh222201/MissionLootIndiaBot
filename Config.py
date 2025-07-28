e499757e4a1f66c4a7a7ed0da0863864f8dcc38d
Nitesh Dubey <152082468+Nitesh222201@users.noreply.github.com>
2025-07-29 04:07:30 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,17 @@
+API_ID = 29843923
+API_HASH = "df8d2c8a1f8d9b8e1c4b70a9f37343f5"
+BOT_TOKEN = "7087907378:AAGbZK5Ff0gxE_GCWoq2cex7Go46i_K0Vdo"
+
+# Source channels (without @)
+SOURCE_CHANNELS = [
+    "GrowDealz",
+    "RAREDEALS2_0",
+    "PrimeSavers",
+    "LOOT_DEAL_INDIA",
+    "MissionIndiaLoot",
+    "OfferZone_2_0",
+    "LootAddaGroup"
+]
+
+# Destination channel
+DESTINATION_CHANNEL = "MissionLootIndia"
